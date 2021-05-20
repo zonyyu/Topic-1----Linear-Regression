8b9ab158bd2c0db21e66d388d3f4fefe3cdc4752
Zony Yu <zony249@gmail.com>
2021-05-20 10:06:33 -0600

    Finished developing topic 1 demo

--- disp_utils.py
+++ disp_utils.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from bokeh.plotting import figure, show, output_file
 from bokeh.io import output_notebook, push_notebook
-from ipywidgets import interact
+from ipywidgets import interact, FloatSlider
 from copy import copy
 
 
@@ -24,6 +24,15 @@ y = 0.05*x
 ref = p2.line(x, y)
 
 
+p3 = figure(width=600, height=400, x_axis_label="Mileage", y_axis_label="Price", title="Price vs Mileage", x_range=(0,500000), y_range=(0, 30000))
+p3.circle(X, Y, x=0, y=0)
+
+x_p = np.linspace(0, 600000, 500)
+y_p = 0.05*x_p - 0.0025*x_p**2 - 0.0003*x_p**3
+
+ref2 = p3.line(x_p, y_p)
+
+
 
 def show_price_vs_mileage(p=p):
     output_notebook()
@@ -38,5 +47,20 @@ def update_graph(w=0.05, b=0):
     x = ref.data_source.data['x']
     ref.data_source.data['y'] = w * x + b
     push_notebook()
-    
 
+def show_poly(p=p3):
+    output_notebook()
+    show(p, notebook_handle=True)
+    interact(update_graph_poly, 
+            w1=FloatSlider(value=1e-3, min=-0.1, max=0.1, 
+                    step=0.001, readout_format='.3f'), 
+            w2=FloatSlider(value=0, min=-10, max=10, 
+                    step=0.001, readout_format='.3f'), 
+            w3=FloatSlider(value=0, min=-10, max=10, 
+                    step=0.0001, readout_format='.5f'), 
+            b=(-50000, 50000))
+
+def update_graph_poly(w1=0.05, w2=-0.0025, w3=-0.0003, b=10000):
+    x = ref2.data_source.data['x']
+    ref2.data_source.data['y'] = w1*x + 1e-7*w2*x**2 + 1e-13*w3*x**3 + b
+    push_notebook()
