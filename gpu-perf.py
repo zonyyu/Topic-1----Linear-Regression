7e423e2329c350e858276d691d722b1704a32a66
Zony Yu <zony249@gmail.com>
2021-05-24 17:24:16 -0600

    Finished. Now add comments

--- gpu-perf.py
+++ gpu-perf.py
@@ -1,6 +1,18 @@
 from email import message
 from flask import Flask, render_template, redirect, url_for, request, jsonify
 
+import numpy as np
+import import_ipynb
+from gpu_perf_predictor_train import Linreg_p
+import torch
+from torch.autograd import Variable
+from sklearn.preprocessing import PolynomialFeatures
+
+gpu_model = Linreg_p(35, 1)
+gpu_model.load_state_dict(torch.load("gpu-predictor.pt"))
+gpu_model.eval()
+
+
 app = Flask(__name__)
 
 
@@ -11,12 +23,20 @@ def home():
 
 @app.route('/test1', methods=['POST'])
 def test1():
-    pnode = request.form.get("pnode")
-    area = request.form.get("area")
-    trans = request.form.get("trans")
+    pnode = request.form.get("pnode") # Fab
+    area = request.form.get("area") # Die Size
+    trans = request.form.get("trans") # Millions Transistors
+    mem = request.form.get("mem") # Memory Size
 
-    print(pnode, area, trans)
-    return jsonify({'pred' : '42'})
+    inputs = np.array([trans, area, mem, pnode]).reshape(1, 4)
+    poly = PolynomialFeatures(degree=3)
+    inputs_p = poly.fit_transform(inputs)
+
+    torch_input = Variable(torch.from_numpy(inputs_p)).float()
+
+    gflops = gpu_model(torch_input)
+    
+    return jsonify({'pred' : "GFLOPS: " + str(gflops.item())})
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ linreg.py
@@ -0,0 +1,4 @@
+import import_ipynb
+from gpu_perf_predictor_train import Linreg
+import torch
+
